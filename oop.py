8233f374364c42f780162c2c1c471dc6237a3410
NadiaPia <206i417@gmail.com>
2024-10-17 14:12:23 -0700

    oop practice

--- oop.py
+++ oop.py
@@ -134,18 +134,90 @@
 #============================================Encapsulation====================================
 #Binding fuctions with the data
 
-class PlayerCharacter:
-    def __init__(self, name, age): 
-        self.name = name
-        self.age = age
+# class PlayerCharacter:
+#     def __init__(self, name, age): 
+#         self.name = name
+#         self.age = age
 
-    def run(self):
-        return self
+#     def run(self):
+#         return self
     
-    def speak(self):
-        print(f'my name is {self.name}, and I am {self.age} years old')
+#     def speak(self):
+#         print(f'my name is {self.name}, and I am {self.age} years old')
 
-player1 = PlayerCharacter('Nadia', 37)
-player1.speak()
+# player1 = PlayerCharacter('Nadia', 37)
+# player1.speak()
+
+#============================================Abstraction====================================
+#hidng not nessecary info - f.e we don't need to know how built in functions work under the hood
+# class PlayerCharacter:
+#     def __init__(self, name, age): 
+#         self.name = name
+#         self.age = age
+
+#     def run(self):
+#         print('run')
+    
+#     def speak(self):
+#         print(f'my name is {self.name}, and I am {self.age} years old')
+
+# player1 = PlayerCharacter('Nadia', 37)
+# player1.speak()
+
+
+#============================================Public and private variables====================================
+
+# class PlayerCharacter:
+#     def __init__(self, name, age): 
+#         self._name = name  #undescore means it is a private and should not be modefied
+#         self._age = age
+
+#     def run(self):
+#         print('run')
+    
+#     def speak(self):
+#         print(f'my name is {self._name}, and I am {self._age} years old')
+
+# player1 = PlayerCharacter('Nadia', 37)
+# player1.name = '!!!!'  # easy to overwrite the code above
+# player1.speak = 'BOOO'
+
+# print(player1.speak)
+
+#   __init__ underscores - Dunder Method - a convention as it should not be modified
+
+#===========================================Inheritance==================================
+#alows new objects to take on the properties of existing objects, so you can inherince classes
+
+class User():
+    def sign_in(self): #no __init__ as we don't need to asign any variables
+        print('logged in')
+
+#Wizards and Archers are users as well, so we need to inherit the sign_in function by passing the User class as an argument
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'attacking with power of {self.power}')
+
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+
+    def attack(self):
+        print(f'attacking with arrows: arrows left - {self.num_arrows}')
+
+wizard1 = Wizard('Merlin', 50)
+archer1 = Archer('Robin', 100)
+wizard1.attack()
+archer1.attack()
+
+#print(wizard1.sign_in()) #Lodded in
+
+
 
 
